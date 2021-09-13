54e227641fe0ea9b14a8c14c3832bef038e7796f
Jet Ysalina <jethercy@gmail.com>
2021-09-12 21:30:05 -0700

    Added file writing to StockBroker.cs (WIP)

--- Lab1/StockBroker.cs
+++ Lab1/StockBroker.cs
@@ -13,10 +13,13 @@ namespace Stock
         public List<Stock> stocks = new List<Stock>();
 
         public static ReaderWriterLockSlim myLock = new ReaderWriterLockSlim();
-        readonly string docPath = @"C:\Users\Documents\CECS 475\Lab1_output.txt";
+
+        readonly string docPath = @"C:\Users\Jether\Documents\CECS 475\Lab1_output.txt";
 
         public string titles = "Broker".PadRight(10) + "Stock".PadRight(15) +
-       "Value".PadRight(10) + "Changes".PadRight(10) + "Date and Time";
+       "Value".PadRight(10) + "Changes".PadRight(10) + "Date and Time\n";
+
+        private bool newFileBool = true;
 
         /// <summary>
         /// The stockbroker object
@@ -40,19 +43,53 @@ namespace Stock
         /// </summary>
         /// <param name="sender">The sender that indicated a change</param>
         /// <param name="e">Event arguments</param>
-        private void EventHandler(Object sender, StockNotification sn)
+        private async void EventHandler(Object sender, StockNotification sn)
         {
             try
             {
                 Stock newStock = (Stock)sender;
                 string statement;
+                string fileData;
+
+                //=== write to console the stock notification info ===
                 statement = BrokerName + " " + sn.StockName + " " + sn.CurrentValue + " " + sn.NumChanges;
                 Console.WriteLine(statement);
 
+                //=== write to file the stock notification info ===
+                DateTime now = DateTime.Now;
+                string time = now.ToString("F"); // "F" specifier creates returns a full date and time string
+                //save the following information to a file when the stock's threshold is reached: date and time, stock name, inital value and current value
+                fileData = BrokerName.PadRight(10) + sn.StockName.PadRight(15) + sn.CurrentValue.ToString().PadRight(10) + sn.NumChanges.ToString().PadRight(10) + time +"\n";
+
+                //if the file exists, overwrites the file and adds the titles to the top of the file
+                if (newFileBool)
+                {
+                    using (StreamWriter writer = new StreamWriter(docPath))
+                    {
+                        await writer.WriteAsync(titles);
+                    }
+                    newFileBool = false;
+                }
+
+                /*var rand = new Random();
+                var sleepTime = rand.Next(0, 1500);*/
+                myLock.EnterWriteLock();
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(docPath, append: true))
+                    {
+                        await writer.WriteAsync(fileData);
+                    }
+                }
+                finally
+                {
+                    myLock.ExitWriteLock();
+                    Thread.Sleep(1000);
+                }
             }
             catch
             {
-
+                Console.WriteLine("error caught");
             }
         }
     }
